7ccd3e5231f2e354d0d2ac3e3cd6230145b6d10e
Gajendra <gajendrkarle@gmail.com>
2018-06-03 14:15:05 -0700

    Create Ifelse BMI calculator.py

---
+++ Ifelse BMI calculator.py
@@ -0,0 +1,12 @@
+BMI = weight_kg / (height_m **2)
+
+print (BMI)
+
+if BMI < 25 :
+    print ("Gaju is healthy")
+elif BMI == 25:
+    print ("Gaju is right on the target")
+elif BMI > 28 :
+    print ("Gaju is over weight")
+else:
+    print( "Check your BMI Gaju")
