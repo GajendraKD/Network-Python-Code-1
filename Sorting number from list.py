29a0bfb3719015c0d3a7a444681dff641438e397
Gajendra <gajendrkarle@gmail.com>
2018-06-03 14:11:26 -0700

    Create Sorting number from list.py

---
+++ Sorting number from list.py
@@ -0,0 +1,13 @@
+a = [2,10,4,5,2,3,5,335,5,6334,897]
+print (a)
+a.sort()
+print (a)
+a.sort(reverse= True)
+print (a)
+
+num1 = [10,20,30,40,50,60,70,80,90,100]
+print (num1)
+num1.sort()
+print (num1)
+num1.sort(reverse=True)
+print (num1)
